728b71b23d765c174785ca02a1f9a38b92c8939d
GuusBaeten-tue <160344365+GuusBaeten-tue@users.noreply.github.com>
2026-03-03 10:22:23 +0100

    create test_network
    create a file that automatically loads the dataset, creates a network, and executes the network with a set amount of epochs.

--- src/test_network.py
+++ src/test_network.py
@@ -4,12 +4,12 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 import network
 
 
-# Create network: 2 inputs, 3 hidden neurons, 1 output
-net = network.Network([784, 30, 10])
+# Create network: 784 inputs, 30 hidden neurons, 10 outputs
+net = network.Network([784, 50, 10])
 
 # Train
 # ETA is the learning rate
 net.SGD(training_data, epochs=30, mini_batch_size=10, eta=3.0, test_data=test_data)
 
-# Epoch 29: 9698 / 10000, took 16.28 seconds
+# Epoch 29: 9698 / 10000
 # run in the src directory with "python3 test_network.py"
